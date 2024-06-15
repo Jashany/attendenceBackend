21848eb36dad4b7f8e3ac542ebbf89139241c3fe
Jashany <jsingh12_be22@thapar.edu>
2024-06-15 21:51:18 +0530

    database added

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const ConnectDB = async () => {
   try {
-    const connection = await mongoose.connect(`mongodb://localhost:27017/attendence`);
+    const connection = await mongoose.connect(`mongodb+srv://jashanmaybe75:zJNuagx59eZ4Imk1@cluster0.uy4nu8u.mongodb.net/attendence`);
     console.log(`MongoDB connected : ${connection.connection.host}`);
   } catch (error) {
     console.log("MONGODB connection FAILED ", error);
